87d17241493d73f4d24aa9f8c3d2b4758ed9800c
ankur <ankurkum46@gmail.com>
2018-04-11 13:30:01 +0530

    Refactor: MainActivity: removed unused code'

--- app/src/main/java/com/buildboard/modules/login/MainActivity.java
+++ app/src/main/java/com/buildboard/modules/login/MainActivity.java
@@ -2,10 +2,8 @@ package com.buildboard.modules.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import com.buildboard.R;
-import com.buildboard.app.MyApplication;
 
 public class MainActivity extends AppCompatActivity {
 
