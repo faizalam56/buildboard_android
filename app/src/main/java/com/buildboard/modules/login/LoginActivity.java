36c2f1e8e70bdd2c4f54ad38fded1c7df796ba49
ankur <ankurkum46@gmail.com>
2018-04-11 17:35:04 +0530

    Refactor: Login: added login module

---
+++ app/src/main/java/com/buildboard/modules/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.buildboard.modules.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.buildboard.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
