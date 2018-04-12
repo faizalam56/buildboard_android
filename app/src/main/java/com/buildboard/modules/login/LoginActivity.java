a0c116f861d28575e534f7b57bd732f0f29989e5
ankur <ankurkum46@gmail.com>
2018-04-12 17:37:42 +0530

    Feature : Login : Added component for login screen

--- app/src/main/java/com/buildboard/modules/login/LoginActivity.java
+++ app/src/main/java/com/buildboard/modules/login/LoginActivity.java
@@ -1,15 +1,69 @@
 package com.buildboard.modules.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.buildboard.R;
+import com.buildboard.utils.FontHelper;
+
+import java.util.ArrayList;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.spinner_usertype)
+    Spinner spinnerUserType;
+
+    @BindView(R.id.edit_username)
+    EditText editUserName;
+    @BindView(R.id.edit_password)
+    EditText editPassword;
+
+    @BindView(R.id.text_forgot_password)
+    TextView textForgotPassword;
+    @BindView(R.id.text_signup)
+    TextView textSignUp;
+
+    @BindView(R.id.button_signin)
+    Button buttonSignIn;
+    @BindView(R.id.button_login_facebook)
+    Button buttonLoginFacebook;
+    @BindView(R.id.button_login_google)
+    Button buttonLoginGoogle;
+
+    @BindString(R.string.contractor)
+    String stringContractor;
+    @BindString(R.string.consumer)
+    String stringConsumer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        setFont();
+        setSpinnerAdapter();
+    }
+
+    private void setSpinnerAdapter() {
+        ArrayList<String> userTypeList = new ArrayList<>();
+        userTypeList.add(stringContractor);
+        userTypeList.add(stringConsumer);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.spinner_usertype_item, userTypeList);
+        spinnerUserType.setAdapter(arrayAdapter);
+    }
+
+    private void setFont() {
+        FontHelper.setFontFace(FontHelper.FontType.FONT_REGULAR, editPassword, editUserName, textForgotPassword, textSignUp,
+                buttonLoginFacebook, buttonLoginGoogle, buttonSignIn);
     }
 }
