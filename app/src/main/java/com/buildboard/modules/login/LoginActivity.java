21929d717397d2c3bf31c9d1fbe341ba68d2b4fd
ankur <ankurkum46@gmail.com>
2018-04-13 12:15:57 +0530

    Refactor: LoginActivity: improved code style

--- app/src/main/java/com/buildboard/modules/login/LoginActivity.java
+++ app/src/main/java/com/buildboard/modules/login/LoginActivity.java
@@ -58,7 +58,6 @@ public class LoginActivity extends AppCompatActivity {
 
         setFont();
         setSpinnerAdapter();
-        isFieldsValid();
     }
 
     private void setSpinnerAdapter() {
