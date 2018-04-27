24b8f6768b064d109dfaf06aa264e21334d8f45f
ankur <ankurkum46@gmail.com>
2018-04-27 14:07:44 +0530

    Feature: drafts: added drafts screen

---
+++ app/src/main/java/com/buildboard/modules/home/modules/drafts/DraftsFragment.java
@@ -0,0 +1,66 @@
+package com.buildboard.modules.home.modules.drafts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.buildboard.R;
+import com.buildboard.modules.home.modules.drafts.adapters.DraftsAdapter;
+import com.buildboard.view.SimpleDividerItemDecoration;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class DraftsFragment extends Fragment {
+
+    @BindView(R.id.recycler_drafts)
+    RecyclerView recyclerDrafts;
+
+    Unbinder unbinder;
+
+    public static DraftsFragment newInstance() {
+        DraftsFragment fragment = new DraftsFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_drafts, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        setServicesRecycler();
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private void setServicesRecycler() {
+        DraftsAdapter selectionAdapter = new DraftsAdapter(getActivity(), getDatas());
+        recyclerDrafts.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerDrafts.addItemDecoration(new SimpleDividerItemDecoration(getActivity()));
+        recyclerDrafts.setAdapter(selectionAdapter);
+    }
+
+    private ArrayList<String> getDatas() {
+        ArrayList<String> datas = new ArrayList<>();
+        datas.add("Service 1");
+        datas.add("Service 2");
+        datas.add("Service 3");
+        datas.add("Service 4");
+        datas.add("Service 5");
+
+        return datas;
+    }
+}
