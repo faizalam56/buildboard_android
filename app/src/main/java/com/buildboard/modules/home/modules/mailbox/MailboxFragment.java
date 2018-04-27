7afd15bd7f2ab7006cf9b1cfcf5beb25fe719eef
ankur <ankurkum46@gmail.com>
2018-04-27 15:47:09 +0530

    Refactor: Mailbox: ui improved, added mailbox icons

--- app/src/main/java/com/buildboard/modules/home/modules/mailbox/MailboxFragment.java
+++ app/src/main/java/com/buildboard/modules/home/modules/mailbox/MailboxFragment.java
@@ -5,11 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.buildboard.R;
-import com.buildboard.modules.home.modules.projects.ProjectsFragment;
+import com.buildboard.fonts.FontHelper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class MailboxFragment extends Fragment {
+
+    @BindView(R.id.text_compose)
+    TextView textCompose;
+    @BindView(R.id.text_inbox)
+    TextView textInbox;
+    @BindView(R.id.text_draft)
+    TextView textDraft;
+    @BindView(R.id.text_bin)
+    TextView textBin;
+    Unbinder unbinder;
 
     public static MailboxFragment newInstance() {
         MailboxFragment fragment = new MailboxFragment();
@@ -19,7 +34,20 @@ public class MailboxFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mailbox, container, false);
+        View view = inflater.inflate(R.layout.fragment_mailbox, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        setFont();
+
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private void setFont() {
+        FontHelper.setFontFace(FontHelper.FontType.FONT_REGULAR, textCompose, textInbox, textDraft, textBin);
+    }
 }
