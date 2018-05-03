0ce4bd81d4cad49870e9161a20ab979d6f9f0ae7
ankur <ankurkum46@gmail.com>
2018-05-03 11:44:12 +0530

    Refactor: LoginActivity:  improved code style

--- app/src/main/java/com/buildboard/modules/login/SocialInterface.java
+++
@@ -1,6 +0,0 @@
-package com.buildboard.modules.login;
-
-public interface SocialInterface {
-//    void success(Task<AuthResult> task, String socialLoginType);
-//    void error(Task<AuthResult> task, String socialLoginType);
-}
