d294d1bc01fdb749c08527a11b920cbee3a62c4e
ankur <ankurkum46@gmail.com>
2018-05-08 17:58:53 +0530

    Refactor: IApiInterface: added access token api method

--- app/src/main/java/com/buildboard/http/IApiInterface.java
+++ app/src/main/java/com/buildboard/http/IApiInterface.java
@@ -1,4 +1,14 @@
 package com.buildboard.http;
 
+import com.buildboard.modules.login.apimodels.GetAccessTokenRequest;
+import com.buildboard.modules.login.apimodels.GetAccessTokenResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface IApiInterface {
+
+    @POST("basic-auth")
+    Call<GetAccessTokenResponse> getAccessToken(@Body GetAccessTokenRequest getAccessTokenRequest);
 }
