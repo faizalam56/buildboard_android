b07f835c1c9af561f62d60b7879c66f1b863da7a
ankur <ankurkum46@gmail.com>
2018-05-17 16:44:39 +0530

    Feature: dialogs: added Popuphelper

---
+++ app/src/main/java/com/buildboard/dialogs/PopUpHelper.java
@@ -0,0 +1,96 @@
+package com.buildboard.dialogs;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import com.buildboard.R;
+
+public class PopUpHelper {
+
+    public interface ConfirmPopUp {
+        void onConfirm(boolean isConfirm);
+        void onDismiss(boolean isDismiss);
+    }
+
+    public interface InfoPopupListener {
+        void onConfirm();
+    }
+
+    public static AlertDialog showConfirmPopup(Activity activity, String message, final ConfirmPopUp confirmPopup) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setCancelable(false);
+        builder.setMessage(message);
+
+        builder.setPositiveButton(activity.getString(R.string.yes), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                confirmPopup.onConfirm(true);
+            }
+        });
+
+        builder.setNegativeButton(activity.getString(R.string.no), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                confirmPopup.onDismiss(false);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        return alertDialog;
+    }
+
+    public static AlertDialog showInfoConfirmPopup(Activity activity, String message, final InfoPopupListener confirmPopup) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setCancelable(false);
+        builder.setMessage(message);
+
+        builder.setPositiveButton(activity.getString(R.string.yes), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                confirmPopup.onConfirm();
+            }
+        });
+
+        builder.setNegativeButton(activity.getString(R.string.no), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        return alertDialog;
+    }
+
+    public static AlertDialog showAlertPopup(Activity activity, String message, final ConfirmPopUp confirmPopup) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                        confirmPopup.onConfirm(true);
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        return alertDialog;
+    }
+
+    public static AlertDialog showInfoAlertPopup(Activity activity, String message, final InfoPopupListener confirmPopup) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                        confirmPopup.onConfirm();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        return alertDialog;
+    }
+}
