450d10319f8332bf158dc7a82774ffceeefbfb67
Sahil <sahiljeets3@gmail.com>
2018-06-29 14:56:15 +0530

    Feature: SignUpContractorActivity: Created UI screens for Contractor Signup

---
+++ app/src/main/java/com/buildboard/modules/signup/Contractor/WorkTypeActivity.java
@@ -0,0 +1,88 @@
+package com.buildboard.modules.signup.Contractor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.buildboard.R;
+import com.buildboard.customviews.BuildBoardTextView;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class WorkTypeActivity extends AppCompatActivity {
+
+    @BindView(R.id.title)
+    TextView title;
+
+    @BindString(R.string.sign_up)
+    String stringSignUp;
+    @BindString(R.string.terms_of_service)
+    String stringTermsOfService;
+    @BindString(R.string.privacy_policy_text)
+    String stringPrivacyPolicy;
+    @BindView(R.id.text_terms_of_service)
+    BuildBoardTextView textTermsOfService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_type);
+        ButterKnife.bind(this);
+
+        title.setText(stringSignUp);
+        setTermsServiceText();
+    }
+
+    @OnClick(R.id.button_next)
+    void nextTapped() {
+        Intent intent = new Intent(this, BusinessDocumentsActivity.class);
+        startActivity(intent);
+    }
+
+    private void setTermsServiceText() {
+        SpannableString styledString = new SpannableString(getString(R.string.privacy_policy_text));
+        styledString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorGreen)), 34, 50, 0);
+        styledString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorGreen)), 55, 69, 0);
+        styledString.setSpan(clickableSpanTermsService, 34, 50, 0);
+        styledString.setSpan(clickableSpanPrivacyPolicy, 55, 69, 0);
+        textTermsOfService.setText(styledString);
+        textTermsOfService.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    ClickableSpan clickableSpanTermsService = new ClickableSpan() {
+        @Override
+        public void onClick(View widget) {
+            Toast.makeText(WorkTypeActivity.this, stringTermsOfService, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setColor(getResources().getColor(R.color.colorGreen));
+        }
+    };
+
+    ClickableSpan clickableSpanPrivacyPolicy = new ClickableSpan() {
+        @Override
+        public void onClick(View widget) {
+            Toast.makeText(WorkTypeActivity.this, stringPrivacyPolicy, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setColor(getResources().getColor(R.color.colorGreen));
+        }
+    };
+}
