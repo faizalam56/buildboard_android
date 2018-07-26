b16438b524ef5926b54f64b76939e581fd4e74b6
ankur <ankurkum46@gmail.com>
2018-07-26 23:43:59 +0530

    Refactor: PreviousWorkActivity: updated ui with recyclerview

--- app/src/main/java/com/buildboard/modules/signup/contractor/previouswork/PreviousWorkActivity.java
+++ app/src/main/java/com/buildboard/modules/signup/contractor/previouswork/PreviousWorkActivity.java
@@ -2,6 +2,8 @@ package com.buildboard.modules.signup.contractor.previouswork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.SpannableString;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
@@ -16,10 +18,13 @@ import com.buildboard.constants.AppConstant;
 import com.buildboard.customviews.BuildBoardTextView;
 import com.buildboard.dialogs.AddProfilePhotoDialog;
 import com.buildboard.http.DataManager;
+import com.buildboard.modules.signup.contractor.businessdocuments.models.DocumentData;
+import com.buildboard.modules.signup.contractor.interfaces.IAddMoreCallback;
+import com.buildboard.modules.signup.contractor.previouswork.adapters.PreviousWorkAdapter;
+import com.buildboard.modules.signup.contractor.previouswork.adapters.TestimonialAdapter;
 import com.buildboard.modules.signup.contractor.previouswork.models.PreviousWorkData;
 import com.buildboard.modules.signup.contractor.previouswork.models.PreviousWorkRequest;
 import com.buildboard.modules.signup.contractor.previouswork.models.PreviousWorks;
-import com.buildboard.modules.signup.contractor.previouswork.models.TestimonialData;
 import com.buildboard.utils.ProgressHelper;
 
 import java.util.ArrayList;
@@ -41,12 +46,22 @@ public class PreviousWorkActivity extends AppCompatActivity implements AppConsta
     String stringTermsOfService;
     @BindString(R.string.privacy_policy_text)
     String stringPrivacyPolicy;
-    @BindView(R.id.text_terms_of_service)
-    BuildBoardTextView textTermsOfService;
     @BindString(R.string.please_wait)
     String stringPleaseWait;
 
+    @BindView(R.id.text_terms_of_service)
+    BuildBoardTextView textTermsOfService;
+
+    @BindView(R.id.recycler_previous_work)
+    RecyclerView recyclerPreviousWork;
+    @BindView(R.id.recycler_testimonial)
+    RecyclerView recyclerTestimonial;
+
     private String mUserId = "";
+    private PreviousWorkAdapter mPreviousWorkAdapter;
+    private TestimonialAdapter mTestimonialAdapter;
+    private HashMap<Integer, ArrayList<PreviousWorkData>> mPreviousWorks = new HashMap<>();
+    private HashMap<Integer, ArrayList<DocumentData>> mTestimonials = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +72,11 @@ public class PreviousWorkActivity extends AppCompatActivity implements AppConsta
         title.setText(stringPreviousWork);
         setTermsServiceText();
         getIntentData();
+
+        addTestimonialData();
+        addPreviousWorkData();
+        setTestimonialAdapter();
+        setPreviousWorkAdapter();
 //        storePrevWork();
     }
 
@@ -135,8 +155,8 @@ public class PreviousWorkActivity extends AppCompatActivity implements AppConsta
     private PreviousWorkRequest getPreviousWorkRequest() {
 
         PreviousWorks previousWorks = new PreviousWorks();
-        previousWorks.setPreviousWork(getPreviousWorkData());
-        previousWorks.setTestimonial(getTestimonialData());
+        previousWorks.setPreviousWork(mPreviousWorks);
+        previousWorks.setTestimonial(mTestimonials);
 
         PreviousWorkRequest previousWorkRequest = new PreviousWorkRequest();
         previousWorkRequest.setPreviousWorks(previousWorks);
@@ -146,37 +166,53 @@ public class PreviousWorkActivity extends AppCompatActivity implements AppConsta
         return previousWorkRequest;
     }
 
-    private HashMap<Integer, ArrayList<TestimonialData>> getTestimonialData() {
-        HashMap<Integer, ArrayList<TestimonialData>> testimonials = new HashMap<>();
+    private void addTestimonialData() {
 
-        for (int i = 1; i <= 2; i++) {
-            ArrayList<TestimonialData> testimonialDetails = new ArrayList<>();
-            TestimonialData testimonialData = new TestimonialData();
-            testimonialData.setKey(KEY_NAME);
-            testimonialData.setType(TYPE_TEXT);
-            testimonialData.setValue("");
-            testimonialDetails.add(testimonialData);
+        ArrayList<DocumentData> testimonialDetails = new ArrayList<>();
+        DocumentData testimonialData = new DocumentData();
+        testimonialData.setKey(KEY_NAME);
+        testimonialData.setType(TYPE_TEXT);
+        testimonialData.setValue("");
+        testimonialDetails.add(testimonialData);
 
-            testimonials.put(i, testimonialDetails);
-        }
-
-        return testimonials;
+        mTestimonials.put(mTestimonials.size() + 1, testimonialDetails);
     }
 
-    private HashMap<Integer, ArrayList<PreviousWorkData>> getPreviousWorkData() {
-        HashMap<Integer, ArrayList<PreviousWorkData>> previousWorks = new HashMap<>();
+    private void addPreviousWorkData() {
 
-        for (int i = 1; i <= 2; i++) {
-            ArrayList<PreviousWorkData> previousWorkDetails = new ArrayList<>();
-            PreviousWorkData testimonialData = new PreviousWorkData();
-            testimonialData.setKey(KEY_NAME);
-            testimonialData.setType(TYPE_TEXT);
-            testimonialData.setValue(new ArrayList<String>());
-            previousWorkDetails.add(testimonialData);
+        ArrayList<PreviousWorkData> previousWorkDetails = new ArrayList<>();
+        PreviousWorkData testimonialData = new PreviousWorkData();
+        testimonialData.setKey(KEY_NAME);
+        testimonialData.setType(TYPE_TEXT);
+        testimonialData.setValue(new ArrayList<String>());
+        previousWorkDetails.add(testimonialData);
 
-            previousWorks.put(i, previousWorkDetails);
-        }
+        mPreviousWorks.put(mPreviousWorks.size() + 1, previousWorkDetails);
+    }
 
-        return previousWorks;
+    private void setPreviousWorkAdapter() {
+        mPreviousWorkAdapter = new PreviousWorkAdapter(this, mPreviousWorks, new IAddMoreCallback() {
+            @Override
+            public void addMore() {
+                addPreviousWorkData();
+                mPreviousWorkAdapter.notifyDataSetChanged();
+            }
+        });
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerPreviousWork.setLayoutManager(linearLayoutManager);
+        recyclerPreviousWork.setAdapter(mPreviousWorkAdapter);
+    }
+
+    private void setTestimonialAdapter() {
+        mTestimonialAdapter = new TestimonialAdapter(this, mTestimonials, new IAddMoreCallback() {
+            @Override
+            public void addMore() {
+                addPreviousWorkData();
+                mTestimonialAdapter.notifyDataSetChanged();
+            }
+        });
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerTestimonial.setLayoutManager(linearLayoutManager);
+        recyclerTestimonial.setAdapter(mTestimonialAdapter);
     }
 }
