ecb9cc1ef55adca5206e8686d7d460f4e7662457
ankur <ankurkum46@gmail.com>
2018-07-26 13:03:53 +0530

    Feature: InsuranceAdapter: get values from fields and set to model

--- app/src/main/java/com/buildboard/modules/signup/contractor/businessdocuments/adapters/InsuranceAdapter.java
+++ app/src/main/java/com/buildboard/modules/signup/contractor/businessdocuments/adapters/InsuranceAdapter.java
@@ -3,16 +3,21 @@ package com.buildboard.modules.signup.contractor.businessdocuments.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.buildboard.R;
+import com.buildboard.customviews.BuildBoardEditText;
 import com.buildboard.customviews.BuildBoardTextView;
 import com.buildboard.fonts.FontHelper;
 import com.buildboard.modules.home.modules.mailbox.draft.drafts_reply.DraftsReplyActivity;
+import com.buildboard.modules.signup.contractor.businessdocuments.GenericTextWatcher;
 import com.buildboard.modules.signup.contractor.businessdocuments.interfaces.IBusinessDocumentsAddMoreCallback;
+import com.buildboard.modules.signup.contractor.businessdocuments.interfaces.ITextWatcherCallback;
 import com.buildboard.modules.signup.contractor.businessdocuments.models.DocumentData;
 
 import java.util.ArrayList;
@@ -44,7 +49,7 @@ public class InsuranceAdapter extends RecyclerView.Adapter<InsuranceAdapter.View
 
     @Override
     public void onBindViewHolder(InsuranceAdapter.ViewHolder holder, int position) {
-        if(position < mInsurances.size()-1)
+        if (position < mInsurances.size() - 1)
             holder.textAddMore.setVisibility(View.GONE);
         else
             holder.textAddMore.setVisibility(View.VISIBLE);
@@ -60,14 +65,47 @@ public class InsuranceAdapter extends RecyclerView.Adapter<InsuranceAdapter.View
         @BindView(R.id.text_add_more)
         BuildBoardTextView textAddMore;
 
+        @BindView(R.id.edit_liability_insurance)
+        BuildBoardEditText editLiability;
+        @BindView(R.id.edit_insurance_provider)
+        BuildBoardEditText editProvider;
+        @BindView(R.id.edit_insurance_amount)
+        BuildBoardEditText editAmount;
+        @BindView(R.id.edit_attachment_insurance)
+        BuildBoardEditText editAttachment;
+
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             setFont();
+
+            editLiability.addTextChangedListener(new GenericTextWatcher(editLiability, new ITextWatcherCallback(){
+
+                @Override
+                public void getValue(String value) {
+                    mInsurances.get(getAdapterPosition() + 1).get(0).setValue(value);
+                }
+            }));
+            editProvider.addTextChangedListener(new GenericTextWatcher(editProvider, new ITextWatcherCallback() {
+                @Override
+                public void getValue(String value) {
+                    mInsurances.get(getAdapterPosition() + 1).get(1).setValue(value);
+                }
+            }));
+            editAmount.addTextChangedListener(new GenericTextWatcher(editAmount, new ITextWatcherCallback() {
+                @Override
+                public void getValue(String value) {
+                    mInsurances.get(getAdapterPosition() + 1).get(2).setValue(value);
+                }
+            }));
+
+            /*editLiability.addTextChangedListener(new GenericTextWatcher(editLiability));
+            editProvider.addTextChangedListener(new GenericTextWatcher(editProvider));
+            editAmount.addTextChangedListener(new GenericTextWatcher(editAmount));*/
         }
 
         @OnClick(R.id.text_add_more)
-        void addmoreTapped(){
+        void addmoreTapped() {
             iAddMoreCallback.addLayout();
         }
 
@@ -75,5 +113,35 @@ public class InsuranceAdapter extends RecyclerView.Adapter<InsuranceAdapter.View
 //            FontHelper.setFontFace(FontHelper.FontType.FONT_BOLD, textName, textTitle, textSubject);
 //            FontHelper.setFontFace(FontHelper.FontType.FONT_LIGHT, textMsg);
         }
+
+        /*private class GenericTextWatcher implements TextWatcher {
+
+            private View view;
+
+            private GenericTextWatcher(View view) {
+                this.view = view;
+            }
+
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            public void afterTextChanged(Editable editable) {
+                String text = editable.toString();
+                switch (view.getId()) {
+                    case R.id.edit_liability_insurance:
+                        mInsurances.get(getAdapterPosition() + 1).get(0).setValue(text);
+                        break;
+                    case R.id.edit_insurance_provider:
+                        mInsurances.get(getAdapterPosition() + 1).get(1).setValue(text);
+                        break;
+                    case R.id.edit_insurance_amount:
+                        mInsurances.get(getAdapterPosition() + 1).get(2).setValue(text);
+                        break;
+                }
+            }
+        }*/
     }
 }
