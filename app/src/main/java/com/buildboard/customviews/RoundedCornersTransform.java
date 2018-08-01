599c1bd1e3873b2e69db59458e83c8dee8f49740
Sahil <sahiljeets3@gmail.com>
2018-08-01 18:32:14 +0530

    Refactor: RoundedCornersTransform: Changed Radius

--- app/src/main/java/com/buildboard/customviews/RoundedCornersTransform.java
+++ app/src/main/java/com/buildboard/customviews/RoundedCornersTransform.java
@@ -29,7 +29,7 @@ public class RoundedCornersTransform implements Transformation {
         paint.setShader(shader);
         paint.setAntiAlias(true);
 
-        float r = size / 8f;
+        float r = size / 12f;
         canvas.drawRoundRect(new RectF(0, 0, source.getWidth(), source.getHeight()), r, r, paint);
         squaredBitmap.recycle();
         return bitmap;
