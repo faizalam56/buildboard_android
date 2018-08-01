3c78b76d5ec989c83ad4b26d958d9ca3fa7964f1
Umesh <ukpatidar88@gmail.com>
2018-08-01 19:49:20 +0530

    refactor: marketplace_profile_signup: Improved  ui and Fixed bug

--- app/src/main/java/com/buildboard/modules/home/modules/projects/ConsumerProjectTypeDetailsFragment.java
+++ app/src/main/java/com/buildboard/modules/home/modules/projects/ConsumerProjectTypeDetailsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,7 @@ import com.buildboard.modules.home.HomeActivity;
 
 import java.util.Objects;
 
-public class ConsumerProjectTypeDetailsFragment extends Fragment {
+public class ConsumerProjectTypeDetailsFragment extends Fragment implements HomeActivity.OnBackPressedListener{
 
     public static ConsumerProjectTypeDetailsFragment newInstance() {
         return new ConsumerProjectTypeDetailsFragment();
@@ -24,6 +25,9 @@ public class ConsumerProjectTypeDetailsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        View view =inflater.inflate(R.layout.fragment_consumer_project_type_details, container, false);
+
+        if (getActivity() != null) ((HomeActivity) getActivity()).setOnBackPressedListener(this);
+
        return  view;
     }
 
@@ -32,5 +36,15 @@ public class ConsumerProjectTypeDetailsFragment extends Fragment {
     public void onResume() {
         super.onResume();
         ((HomeActivity) Objects.requireNonNull(getActivity())).setTitle(getString(R.string.select_location));
+    }
+
+    @Override
+    public void doBack() {
+        navigateFragment(ConsumerProjectTypeFragment.newInstance());
+    }
+
+    private void navigateFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_home_container, fragment).commit();
     }
 }
