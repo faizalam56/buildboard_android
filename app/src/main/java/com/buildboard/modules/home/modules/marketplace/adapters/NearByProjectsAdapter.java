8b126cbc1b3518d479c6e7ef9cb7d63f742704db
ShubhamKotnala <shubhamkotnala664@gmail.com>
2018-08-02 18:53:03 +0530

    Refactor: Improved MarketPlace UI

--- app/src/main/java/com/buildboard/modules/home/modules/marketplace/adapters/NearByProjectsAdapter.java
+++ app/src/main/java/com/buildboard/modules/home/modules/marketplace/adapters/NearByProjectsAdapter.java
@@ -2,27 +2,28 @@ package com.buildboard.modules.home.modules.marketplace.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.buildboard.R;
 import com.buildboard.fonts.FontHelper;
-import com.buildboard.interfaces.IRecyclerItemClickListener;
-import com.buildboard.modules.home.modules.marketplace.contractors.ContractorsActivity;
 import com.buildboard.modules.home.modules.marketplace.contractors.NearByProjectsActivity;
 import com.buildboard.modules.home.modules.marketplace.models.NearByProjects;
+import com.buildboard.utils.ConnectionDetector;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 import static com.buildboard.constants.AppConstant.DATA;
-import static com.buildboard.constants.AppConstant.INTENT_TITLE;
 
 public class NearByProjectsAdapter extends RecyclerView.Adapter<NearByProjectsAdapter.ViewHolder> {
 
@@ -62,6 +63,8 @@ public class NearByProjectsAdapter extends RecyclerView.Adapter<NearByProjectsAd
         ImageView imageService;
         @BindView(R.id.textRatingBar)
         TextView textRatingbar;
+        @BindView(R.id.card_service)
+        CardView mCoordinatorLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -83,10 +86,16 @@ public class NearByProjectsAdapter extends RecyclerView.Adapter<NearByProjectsAd
 
         @OnClick(R.id.card_service)
         public void rowTapped() {
-            NearByProjects nearByProjects = mNearByProjects.get(getAdapterPosition());
-            Intent intent = new Intent(mContext, NearByProjectsActivity.class);
-            intent.putExtra(DATA, nearByProjects.getId());
-            mContext.startActivity(intent);
+
+            if (ConnectionDetector.isNetworkConnected(mContext)) {
+                NearByProjects nearByProjects = mNearByProjects.get(getAdapterPosition());
+                Intent intent = new Intent(mContext, NearByProjectsActivity.class);
+                intent.putExtra(DATA, nearByProjects.getId());
+                mContext.startActivity(intent);
+            } else {
+                ConnectionDetector.createSnackBar(mContext, mCoordinatorLayout);
+            }
+
         }
     }
  }
