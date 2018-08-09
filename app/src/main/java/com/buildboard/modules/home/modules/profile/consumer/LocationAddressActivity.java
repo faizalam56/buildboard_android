8b720d92441d829372c3fbc2ff1ed7914e39dfeb
Sahil <sahiljeets3@gmail.com>
2018-08-09 14:54:21 +0530

    Refactor: LocationAddressActivity: Changed trash icon to white

--- app/src/main/java/com/buildboard/modules/home/modules/profile/consumer/LocationAddressActivity.java
+++ app/src/main/java/com/buildboard/modules/home/modules/profile/consumer/LocationAddressActivity.java
@@ -202,14 +202,14 @@ public class LocationAddressActivity extends AppCompatActivity
                         p.setColor(Color.parseColor("#D32F2F"));
                         RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX, (float) itemView.getBottom());
                         c.drawRect(background, p);
-                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_bin);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_trash);
                         RectF icon_dest = new RectF((float) itemView.getLeft() + width, (float) itemView.getTop() + width, (float) itemView.getLeft() + 2 * width, (float) itemView.getBottom() - width);
                         c.drawBitmap(icon, null, icon_dest, p);
                     } else if (dX < 0) {
                         p.setColor(Color.parseColor("#D32F2F"));
                         RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
                         c.drawRect(background, p);
-                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_bin);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_trash);
                         RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
                         c.drawBitmap(icon, null, icon_dest, p);
                     }
