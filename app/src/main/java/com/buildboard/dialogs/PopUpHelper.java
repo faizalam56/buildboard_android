fe5cd0c1e29ecaa55e0a0d5071af7570b8c6bb24
Sahil <sahiljeets3@gmail.com>
2018-08-14 12:35:59 +0530

    Feature: LocationAddressActivity: Added popup for edit address

--- app/src/main/java/com/buildboard/dialogs/PopUpHelper.java
+++ app/src/main/java/com/buildboard/dialogs/PopUpHelper.java
@@ -1,9 +1,19 @@
 package com.buildboard.dialogs;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.buildboard.R;
+import com.buildboard.customviews.BuildBoardEditText;
+import com.buildboard.customviews.BuildBoardTextView;
+import com.buildboard.view.SnackBarFactory;
+import com.google.android.gms.location.places.Place;
 
 public class PopUpHelper {
 
@@ -14,6 +24,10 @@ public class PopUpHelper {
 
     public interface InfoPopupListener {
         void onConfirm();
+    }
+
+    public interface EditAddressListener {
+        void onConfirm(String updatedAddress);
     }
 
     public static AlertDialog showConfirmPopup(Activity activity, String message, final ConfirmPopUp confirmPopup) {
@@ -91,5 +105,32 @@ public class PopUpHelper {
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
         return alertDialog;
+    }
+
+    public static void showAddressDialog(final Activity activity, Place place, final EditAddressListener editAddressListener) {
+        if (place != null) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+            LayoutInflater inflater = activity.getLayoutInflater();
+            @SuppressLint("InflateParams") final View dialogView = inflater.inflate(R.layout.dialog_custom_places, null);
+            dialogBuilder.setView(dialogView);
+            final BuildBoardEditText buildBoardEditText = dialogView.findViewById(R.id.editPlaceName);
+            final BuildBoardTextView textView = dialogView.findViewById(R.id.textSelectedLocation);
+            buildBoardEditText.setText(place.getName());
+            textView.setText(place.getAddress());
+            dialogBuilder.setTitle(activity.getString(R.string.confirm_location));
+            dialogBuilder.setPositiveButton(activity.getString(R.string.done), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    editAddressListener.onConfirm(buildBoardEditText.getText().toString());
+                }
+            });
+            dialogBuilder.setNegativeButton(activity.getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog alertChangeAddressDialog = dialogBuilder.create();
+            alertChangeAddressDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            alertChangeAddressDialog.show();
+        }
     }
 }
